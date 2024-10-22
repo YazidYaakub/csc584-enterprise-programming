80dc66f90f888065499a4c23d1f2da9b2cc143b9
Harith Iqbal <hrithiqball@gmail.com>
2024-10-22 23:22:36 +0800

    feat: bootstrapped app with java spring

---
+++ transtrack/src/test/java/com/example/transtrack/TranstrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.transtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranstrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
