cb0c5ef133b6d1b3652a4f75cf3827be08c38043
taufikcharlie <mohamadtaufik099@gmail.com>
2024-12-06 13:08:15 +0800

    feat: StudentConsultant Model without join

--- interntrack/src/main/java/uitm/interntrack/entity/StudentConsultant.java
+++ interntrack/src/main/java/uitm/interntrack/entity/StudentConsultant.java
@@ -4,16 +4,54 @@ import jakarta.persistence.*;
 import java.sql.Timestamp;
 
 @Entity
-@Table(name = "STUDENT_CONSULTANTS")
+@Table(name = "STUDENT_CONSULTANTS", schema = "INTERNTRACK")
 public class StudentConsultant {
 
+    // @Id
+    // @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    // private Long userId;
+    
+
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq_gen")
+    @SequenceGenerator(name = "user_seq_gen", sequenceName = "STUDENT_CONSULTANT_SEQUENCE", allocationSize = 1)
+    @Column(name = "STUDENT_CONSULTANT_ID")
+    
+    private Long studentConsultantId;
+
+    @Column(name = "STUDENT_ID")
     private Long studentId;
 
+    @Column(name = "ADVISOR_ID")
     private Long advisorId;
+
+    @Column(name = "SUPERVISOR_ID")
     private Long supervisorId;
+
+    @Column(name = "ASSIGNED_AT")
     private Timestamp assignedAt;
+    
+    // private Long studentId;
+    // private Long advisorId;
+    // private Long supervisorId;
+    // private Timestamp assignedAt;
+
+    public StudentConsultant() {
+    }
+
+    public StudentConsultant(Long studentId, Long advisorId, Long supervisorId, Timestamp assignedAt) {
+        this.studentId = studentId;
+        this.advisorId = advisorId;
+        this.supervisorId = supervisorId;
+        this.assignedAt = assignedAt;
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        if (assignedAt == null) {
+            assignedAt = new Timestamp(System.currentTimeMillis());
+        }
+    }
 
     public Long getStudentId() {
         return studentId;
