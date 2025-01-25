0af5bc8a1e7f355f2a0204e0d8fcd05fabdb1395
taufikcharlie <mohamadtaufik099@gmail.com>
2025-01-26 02:51:57 +0800

    update: update company,university,user seeder

--- interntrack/src/main/resources/db/seeder/1-CompanySeeder.sql
+++ interntrack/src/main/resources/db/seeder/1-CompanySeeder.sql
@@ -1,18 +1,18 @@
 -- start companies seeder
 INSERT INTO COMPANIES (COMPANY_ID, NAME, SECTOR, LOCATION, CODE, EMAIL, WEBSITE, LATITUDE, LONGITUDE, CONTACT_NUMBER, LOGO_LINK)
 VALUES
-('1a2b3c4d-5e6f-7g8h-9i0j1k2l3maa', 'Google', 'Technology', 'New York', 'G123', 'contact@googleUITM.com', 'https://www.googleUITM.com', 40.712776, -74.005974, '123-456-7890', 'https://www.googleUITM.com/logo.png');
+('1a2b3c4d-5e6f-7g8h-9i0j1k2l3aaa', 'TM', 'Technology', 'Bangsar', 'TM001', 'contact@tm.com', 'https://www.tm.com.my/', 3.116134, 101.664881, '+60322409494', 'https://www.tm.com/logo.png');
 
 INSERT INTO COMPANIES (COMPANY_ID, NAME, SECTOR, LOCATION, CODE, EMAIL, WEBSITE, LATITUDE, LONGITUDE, CONTACT_NUMBER, LOGO_LINK)
 VALUES
-('1a2b3c4d-5e6f-7g8h-9i0j1k2l3mab', 'Facebook', 'Technology', 'New York', 'FB123', 'contact@facebookUITM.com', 'https://www.facebookUITM.com', 40.712776, -74.005974, '123-456-7890', 'https://www.facebookUITM.com/logo.png');
+('1a2b3c4d-5e6f-7g8h-9i0j1k2l3aab', 'Petronas', 'Oil & Gas', 'Kuala Lumpur City Centre', 'PTR002', 'contact@petronas.com', 'https://www.petronas.com/', 3.157764, 101.711861, '+60320515000', 'https://www.petronas.com/logo.png');
 
 INSERT INTO COMPANIES (COMPANY_ID, NAME, SECTOR, LOCATION, CODE, EMAIL, WEBSITE, LATITUDE, LONGITUDE, CONTACT_NUMBER, LOGO_LINK)
 VALUES
-('1a2b3c4d-5e6f-7g8h-9i0j1k2l3mac', 'Amazon', 'Technology', 'New York', 'A123', 'contact@amazonUITM.com', 'https://www.amazonUITM.com', 40.712776, -74.005974, '123-456-7890', 'https://www.amazonUITM.com/logo.png');
+('1a2b3c4d-5e6f-7g8h-9i0j1k2l3aac', 'Astro', 'Multimedia', 'Bukit Jalil', 'AST003', 'contact@astro.com', 'https://corporate.astro.com.my/', 3.0546, 101.6879, '+60395436688', 'https://www.astro.com/logo.png');
 
 INSERT INTO COMPANIES (COMPANY_ID, NAME, SECTOR, LOCATION, CODE, EMAIL, WEBSITE, LATITUDE, LONGITUDE, CONTACT_NUMBER, LOGO_LINK)
 VALUES
-('1a2b3c4d-5e6f-7g8h-9i0j1k2l3mad', 'Microsoft', 'Technology', 'New York', 'MS123', 'contact@microsoftUITM.com', 'https://www.microsoftUITM.com', 40.712776, -74.005974, '123-456-7890', 'https://www.microsoftUITM.com/logo.png');
+('1a2b3c4d-5e6f-7g8h-9i0j1k2l3aad', 'Unifi', 'Technology', 'Kuala Lumpur', 'UNI004', 'contact@unifi.com', 'https://unifi.com.my/', 3.116134, 101.664881, '+60379332400', 'https://www.unifi.com/logo.png');
 COMMIT;
 -- end companies seeder
